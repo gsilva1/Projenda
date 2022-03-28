c6e0e1e9918c85a076ce3cb38aa438242c7eb5ff
Axel Felix <54403767+AxelVarreira@users.noreply.github.com>
2022-03-28 10:53:58 -0300

    Add files via upload

---
+++ Projenda/src/main/java/com/ifrs/edu/SAO/SaoApplication.java
@@ -0,0 +1,13 @@
+package com.ifrs.edu.SAO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaoApplication.class, args);
+	}
+
+}
---
+++ Projenda/src/test/java/com/ifrs/edu/SAO/SaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifrs.edu.SAO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
